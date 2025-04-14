1ade189679e764dd91ac7b74908528dfb3e000a0
ZygoteCode <zygotecode@proton.me>
2025-04-14 13:37:13 +0200

    Updates to repository

---
+++ Project/Obfusio Core/Utils/Utils.cs
@@ -0,0 +1,26 @@
+﻿using dnlib.DotNet;
+
+public class Utils
+{
+    private static SuperRandom _random = new SuperRandom(2);
+    private static char[] _characters = "ABCDEFGHIJKLMNOQPRSTUVWXYZ0123456789".ToCharArray();
+
+    public static string GenerateName()
+    {
+        return _random.GetRandomString(_characters, 8);
+    }
+
+    public static bool IsValidManagedAssembly(string inputFilePath)
+    {
+        try
+        {
+            return ModuleDefMD.Load(inputFilePath).Types.Count > 0;
+        }
+        catch
+        {
+
+        }
+
+        return false;
+    }
+}
